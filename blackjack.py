b551fdc1e7d308bfd9893760716bff40e0581723
Petar Dobrinov <petardobrinov95@gmail.com>
2019-07-20 19:59:00 +0300

    Spaggheti blackjack

--- blackjack.py
+++ blackjack.py
@@ -3,8 +3,43 @@ import Deck
 import Hand
 import blackjack_functions
 import variables
-while True:
-    balance = Chip.Chip()
+def game_on (player, dealer, deck, bet):
+    while variables.playing:
+        print '\n'
+        res = blackjack_functions.hit_or_stand(deck,player)
+        if res == 'h':
+            blackjack_functions.clear()
+            blackjack_functions.show_some(player, dealer)
+        if player.value > 21:
+            print "\n"
+            blackjack_functions.player_busts(balance, bet)
+            break
+        if res == 's':
+            while dealer.value < 17:
+                blackjack_functions.hit(deck, dealer)
+            blackjack_functions.clear()
+            blackjack_functions.show_all(player, dealer)
+            print "\n"
+            if 21 - player.value < 21 - dealer.value:
+                blackjack_functions.player_wins(balance, bet)
+                break
+            elif dealer.value > 21:
+                blackjack_functions.dealer_busts(balance, bet)
+                break
+            elif 21 - dealer.value < 21 - player.value:
+                blackjack_functions.dealer_wins(balance, bet)
+                break
+            elif dealer.value == player.value:
+                blackjack_functions.push()
+                break
+    if balance.total <= 0:
+        return False
+    repl = blackjack_functions.replay(balance)
+    if repl == 'y':
+        return True
+    else:
+        return False
+def start(balance):
     while True:
         blackjack_functions.clear()
         print 'Your balance is {}$'.format(balance.total)
@@ -29,44 +64,25 @@ while True:
         player.add_card(deck.deal())
         player.add_card(deck.deal())
         blackjack_functions.show_some(player, dealer)
-        while variables.playing:
-            print '\n'
-            res = blackjack_functions.hit_or_stand(deck,player)
-            if res == 'h':
-                blackjack_functions.clear()
-                blackjack_functions.show_some(player, dealer)
-            if player.value > 21:
-                print "\n"
-                blackjack_functions.player_busts(balance, bet)
-                break
-            if res == 's':
-                while dealer.value < 17:
-                    blackjack_functions.hit(deck, dealer)
-                blackjack_functions.clear()
-                blackjack_functions.show_all(player, dealer)
-                print "\n"
-                if 21 - player.value < 21 - dealer.value:
-                    blackjack_functions.player_wins(balance, bet)
-                    break
-                elif dealer.value > 21:
-                    blackjack_functions.dealer_busts(balance, bet)
-                    break
-                elif 21 - dealer.value < 21 - player.value:
-                    blackjack_functions.dealer_wins(balance, bet)
-                    break
-                elif dealer.value == player.value:
-                    blackjack_functions.push()
-                    break
-        if balance.total <= 0:
-            break
-        repl = blackjack_functions.replay(balance)
-        if repl == 'y':
+        game = game_on(player, dealer, deck, bet)
+        if game:
+            return True
+        else:
+            return False
+def mother():
+    while True:
+        global balance
+        balance = Chip.Chip()
+        beginning = start(balance)
+        if beginning:
             continue
         else:
             break
-    print 'You are out of money'
-    new = blackjack_functions.new_game()
-    if new == 'y':
-        continue
-    else:
-        break
+        print 'You are out of money'
+        new = blackjack_functions.new_game()
+        if new == 'y':
+            continue
+        else:
+            break
+if __name__ == '__main__':
+    mother()
